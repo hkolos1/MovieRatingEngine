41a8b4d1c3673c1bcbb2a51d14c8f0d67c9e2eb3
Harun Kološ <hkolos1@etf.unsa.ba>
2022-06-08 12:19:53 +0200

    Movie and User Models

---
+++ MovieRatingEngine/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieRatingEngine.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string PhotoUrl { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal AverageRating { get; set; }
+        public string Type { get; set; }
+        public DateTime Released { get; set; }
+        public List<string> Cast { get; set; }
+
+    }
+}
---
+++ MovieRatingEngine/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieRatingEngine.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
