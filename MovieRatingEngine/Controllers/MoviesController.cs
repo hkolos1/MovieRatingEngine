856613a7db88a8ceb2d131c4d0af6e72dc5dee00
Harun Kološ <hkolos1@etf.unsa.ba>
2022-06-08 16:33:12 +0200

    Start work on GetAllMovies

--- MovieRatingEngine/Controllers/MoviesController.cs
+++ MovieRatingEngine/Controllers/MoviesController.cs
@@ -1,7 +1,32 @@
-﻿namespace MovieRatingEngine.Controllers
-{
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MovieRatingEngine.Services;
+using MovieRatingEngine.Dtos;
+using MovieRatingEngine.Models;
 
-    public class MoviesController
+namespace MovieRatingEngine.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class MoviesController : ControllerBase
     {
+        private readonly IMoviesService _moviesService;
+        public MoviesController(IMoviesService movieService)
+        {
+            _moviesService = movieService;
+        }
+
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<ServiceResponse<List<GetMovieDto>>>> Get()
+        {
+            return Ok(await _moviesService.GetAllMovies());
+        }
     }
 }
---
+++ MovieRatingEngine/Models/ServiceResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieRatingEngine.Models
+{
+    public class ServiceResponse<T>
+    {
+        public T Data { get; set; }
+
+        public bool Success { get; set; } = true;
+
+        public string Message { get; set; } = null;
+
+    }
+}
