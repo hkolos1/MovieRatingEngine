a81d6427c14b67e73273cb36a0314d6ed460a115
Harun Kološ <hkolos1@etf.unsa.ba>
2022-06-09 09:08:20 +0200

    Morning sync

--- MovieRatingEngine/Startup.cs
+++ MovieRatingEngine/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using MovieRatingEngine.Data;
+using MovieRatingEngine.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,6 +37,10 @@ namespace MovieRatingEngine
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MovieRatingEngine", Version = "v1" });
             });
+            services.AddScoped<IMoviesService, MoviesService>();
+            services.AddAutoMapper(typeof(Startup));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
